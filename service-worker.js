21320e9cfaba300f8ad53665e62fa81a0ea14bcb
fyrus7 <88237948+fyrus7@users.noreply.github.com>
2024-09-20 02:19:26 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,18 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker Installed');
+// service-worker.js
+
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installing...');
 });
 
-self.addEventListener('fetch', (e) => {
-  console.log('Service Worker Fetch', e.request.url);
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activating...');
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
 });
